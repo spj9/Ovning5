385dd94523b8d40481709db5f5125a11736558b8
spj9 <sanny_j@hotmail.com>
2024-06-18 23:49:07 +0200

    Added folders and classes

--- Ovning5-Garage/Garage.cs
+++ Ovning5-Garage/Garage.cs
@@ -1,13 +1,88 @@
-ï»¿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace Ovning5_Garage
+// Klass Garage<T> implementerar IEnumerable-gränssnittet och tillhandahåller en generisk samling av fordon.
+class Garage<T> : IEnumerable
+    where T : IVehicle
 {
-    public class Garage
-    {
+    private T?[] _storage;
 
+    public Garage(uint capacity)
+    {
+        _storage = new T[capacity];
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (var item in _storage)
+        {
+            if (item != null)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public void PerformOnAll(Action<T> action)
+    {
+        foreach (var item in _storage)
+        {
+            if (item != null)
+            {
+                action?.Invoke(item);
+            }
+        }
+    }
+
+    public int Add(T vehicle) => _storage.Push(vehicle);
+
+    public int Remove(string regNr)
+    {
+        int idxToRemove = Array.IndexOf(
+            _storage.Select(v => v?.RegNr.ToUpper()).ToArray(),
+            regNr.ToUpper()
+        );
+
+        if (idxToRemove != -1)
+        {
+            _storage[idxToRemove] = default(T);
+        }
+
+        return idxToRemove;
+    }
+
+    public T? Find(string regNr) =>
+        _storage.FirstOrDefault(v =>
+            v?.RegNr.Equals(regNr, StringComparison.OrdinalIgnoreCase) ?? false
+        );
+
+    internal void Populate(T[] vehicles)
+    {
+        for (int i = 0; i < vehicles.Length; i++)
+        {
+            Add(vehicles[i]);
+        }
+    }
+
+    internal IEnumerable<T?> SearchByProps(string vehicleType, string color, uint? wheelCount)
+    {
+        var firstSelection = vehicleType.Equals("any", StringComparison.OrdinalIgnoreCase)
+            ? _storage
+            : _storage.Where(v =>
+                v != null
+                && v.GetType().Name.Equals(vehicleType, StringComparison.OrdinalIgnoreCase)
+            );
+        var secondSelection = color.Equals("any", StringComparison.OrdinalIgnoreCase)
+            ? firstSelection
+            : firstSelection.Where(v =>
+                v != null && v.Color.Equals(color, StringComparison.OrdinalIgnoreCase)
+            );
+        var thirdSelection =
+            wheelCount == null
+                ? secondSelection
+                : secondSelection.Where(v => v != null && v.WheelCount == wheelCount);
+
+        return thirdSelection;
     }
 }
--- Ovning5-Garage/Program.cs
+++ Ovning5-Garage/Program.cs
@@ -1,6 +1,16 @@
-﻿public class Program
+﻿// Övning 5 - Garage
+// Detta är en konsolbaserad applikation som tar hjälp av ett kommandoradsgränssnitt (CLI).
+// ConsoleUI representerar vyn, ansvarig för att hantera användarinmatning och utdata.
+// GarageHandler representerar modellen, ansvarig för att hantera logiken och data relaterade till garaget.
+// Manager representerar controllern, ansvarig för att orkestrera interaktionen mellan vyn och modellen.
+
+internal class Program
 {
-
-
+    private static void Main(string[] args)
+    {
+        var cli = new ConsoleUI();
+        var handler = new GarageHandler();
+        var manager = new Manager(cli, handler);
+        manager.Run();
+    }
 }
-
